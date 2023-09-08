346fe12e1031d54a3dc0c9e1af13a0973e1942d5
Fukuemon <Fukuemony029@gmail.com>
2023-09-09 00:28:49 +0900

    🎨  ✨  現在地を取得し、現在地付近の飲食店を検索する処理の追加とリファクタリング #4
    初回レンダリング時に、GeolocationAPIを用いて現在地を取得し、location変数にセットする
    パラメータを更新する際に、現在のパラメータを受け取っていたため、useSearchParamsを削除し、パラメーターを変更できるように

--- src/app/components/Post/Search/index.tsx
+++ src/app/components/Post/Search/index.tsx
@@ -1,36 +1,51 @@
 "use client";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { FC, useCallback, useEffect, useState, ChangeEvent } from "react";
 
-export const SearchShop = () => {
+interface SearchParams {
+  keyword?: string;
+  range?: string;
+  lat?: string;
+  lng?: string;
+}
+
+export const SearchShop: FC = () => {
   const router = useRouter();
-  const pathname = usePathname(); //現在のパスを取得
-  const searchParams = useSearchParams()!; //現在のsearchParams(パラメーター)を取得
-
-  //searchParams(パラメーター)を受けとり、新しいsearchParams文字列を取得する。
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const query = new URLSearchParams(searchParams);
-      query.set(name, value);
-
-      return query.toString();
-    },
-    [searchParams]
+  const pathname = usePathname(); // 現在のパスを取得
+  // 現在地の緯度経度
+  const [location, setLocation] = useState<{ lat: number; lng: number } | null>(
+    null
   );
+  const [searchText, setSearchText] = useState(""); // 検索テキスト
 
-  //キーワード入力テキスト
-  const [searchText, setSearchText] = useState("");
+  // 現在地の緯度経度を取得する
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
 
-  /**
-   * キーワード入力を取得
-   */
-  const handleSearchInput = (e: any) => {
+  // 検索パラメータを作成する
+  const createQueryString = useCallback((params: SearchParams) => {
+    const query = new URLSearchParams();
+    Object.entries(params).forEach(([key, value]) => {
+      if (value) query.set(key, value);
+    });
+    return query.toString();
+  }, []);
+
+  // 検索テキストを更新する
+  const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value);
   };
 
   return (
     <div className="flex flex-wrap lg:flex-nowrap justify-center pb-8 lg:justify-start">
-      {/* キーワード */}
       <div className="w-[100%] lg:w-[35%] lg:ml-2 mt-1 lg:mt-0">
         <input
           type="text"
@@ -41,16 +56,32 @@ export const SearchShop = () => {
         />
       </div>
 
-      {/* 検索 */}
+      {/* 検索ボタン */}
       <button
         onClick={() => {
-          router.push(
-            pathname + "?" + createQueryString("keyword", searchText)
-          );
+          const params: SearchParams = {
+            keyword: searchText || undefined,
+          };
+          router.push(pathname + "?" + createQueryString(params));
         }}
-        className="rounded-md mt-2 lg:mt-0 lg:ml-2 w-[100%] lg:w-[100px] bg-[#017D01] text-white py-[10px] border-0 text-[14px] max-w-[400px] shadow shrink-0"
+        className="rounded-md mt-2 hover:bg-lime-900 lg:mt-0 lg:ml-2 w-[100%] lg:w-[100px] bg-[#017D01] text-white py-[10px] border-0 text-[14px] max-w-[400px] shadow shrink-0"
       >
-        検索する
+        キーワードで検索する
+      </button>
+
+      {/* 現在地で検索ボタン */}
+      <button
+        onClick={() => {
+          const params: SearchParams = {
+            lat: location?.lat.toString(),
+            lng: location?.lng.toString(),
+            range: "5",
+          };
+          router.push(pathname + "?" + createQueryString(params));
+        }}
+        className="rounded-md mt-2 hover:bg-lime-900 lg:mt-0 lg:ml-2 w-[100%] lg:w-[100px] bg-[#017D01] text-white py-[10px] border-0 text-[14px] max-w-[400px] shadow shrink-0"
+      >
+        現在地で検索する
       </button>
     </div>
   );
--- src/app/post/search/page.tsx
+++ src/app/post/search/page.tsx
@@ -1,36 +1,55 @@
 import { SearchShop } from "@/app/components/Post/Search";
 import Navbar from "@/app/components/elements/Navbar";
 
+interface SearchParams {
+  keyword?: string;
+  range?: number;
+  lat?: number;
+  lng?: number;
+}
+
+// hotpepper APIのエンドポイント
 const defaultEndpoint = `${process.env.NEXT_PUBLIC_HOTPEPPER_API}?key=${process.env.NEXT_PUBLIC_HOTPEPPER_API_KEY}&format=json&count=10`;
 
+// クエリパラメータをを受け取るSearchPage(searchParams)
 export default async function SearchShopPage({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: SearchParams;
 }) {
-  const keyword = searchParams.keyword;
-  let apiKeyword = "";
-  if (keyword !== null) {
-    apiKeyword = `&keyword=${keyword}`;
-  }
+  //　各パラメータを取得
+  const { keyword, lat, lng, range } = searchParams;
 
-  const res = await fetch(`${defaultEndpoint}${apiKeyword}`);
+  // APIパラメータを作成
+  const apiParams = new URLSearchParams();
+
+  // 各パラメータをセット
+  if (keyword) apiParams.append("keyword", keyword); // keywordがあればセット
+
+  // latとlngがあればセット
+  if (lat !== undefined && lng !== undefined) {
+    apiParams.append("lat", lat.toString());
+    apiParams.append("lng", lng.toString());
+  }
+  // rangeがあればセット
+  if (range !== undefined) apiParams.append("range", range.toString());
+
+  // APIを叩く
+  const res = await fetch(`${defaultEndpoint}&${apiParams.toString()}`);
   const data = await res.json();
-  console.log(apiKeyword);
-  console.log(data);
   const shops = data.results.shop;
-  console.log(shops);
+
   return (
     <div>
       <Navbar title="店舗検索" />
-      <div className="p-8 ">
+      <div className="p-8">
         <SearchShop />
         <div>
           {!shops ? (
             <p>検索結果に当てはまりませんでした</p>
           ) : (
             shops.map((shop: any, index: number) => (
-              <div key={index}>
+              <div key={shop.id || index}>
                 <h2 className="text-lg py-2 font-bold">{shop.name}</h2>
               </div>
             ))
