dceab860aedf42e4f0d8553c20763f1fbf0957ba
Fukuemon <Fukuemony029@gmail.com>
2023-09-15 23:57:41 +0900

    💬  カスタムフックにコメントを追加 #1

--- src/app/components/Post/Search/index.tsx
+++ src/app/components/Post/Search/index.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { usePathname, useRouter } from "next/navigation";
-import { FC, useCallback, useEffect, useState, ChangeEvent } from "react";
+import { FC, useState, ChangeEvent } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import InputWithButton from "../../elements/InputWithButtonIcon";
 import { Button } from "@/components/ui/button";
+import useLocation from "./hooks/useLocation";
+import useQueryString from "./hooks/useQueryString";
 
 interface SearchParams {
   keyword?: string;
@@ -12,67 +14,48 @@ interface SearchParams {
   lng?: string;
 }
 
+// 検索ボックス
 export const SearchShop: FC = () => {
   const router = useRouter();
   const pathname = usePathname(); // 現在のパスを取得
-  // 現在地の緯度経度
-  const [location, setLocation] = useState<{ lat: number; lng: number } | null>(
-    null
-  );
-  const [searchText, setSearchText] = useState(""); // 検索テキスト
+  const location = useLocation(); // 現在地を取得するカスタムフック
+  const createQueryString = useQueryString(); // クエリパラメータを作成するカスタムフック
 
-  // 現在地の緯度経度を取得する
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLocation({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      });
-    }
-    handlePushLocation();
-  }, []);
+  const [searchText, setSearchText] = useState("");
 
-  // パラメータを作成する
-  const createQueryString = useCallback((params: SearchParams) => {
-    const query = new URLSearchParams();
-    Object.entries(params).forEach(([key, value]) => {
-      if (value) query.set(key, value);
-    });
-    return query.toString();
-  }, []);
+  // クエリパラメータを作成してrouter.pushする
+  const pushToRouter = (params: SearchParams) => {
+    router.push(pathname + "?" + createQueryString(params));
+  };
 
-  // 検索テキストを更新する
+  // 検索ボックスの入力値をstateに保存
   const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value);
   };
 
   // 検索ボタンを押したときの処理
   const handlePushSearch = () => {
-    const params: SearchParams = {
-      keyword: searchText || undefined,
-    };
-    router.push(pathname + "?" + createQueryString(params));
+    pushToRouter({ keyword: searchText || undefined });
   };
 
+  // 現在地付近の飲食店を検索する
   const handlePushLocation = () => {
-    const params: SearchParams = {
-      lat: location?.lat.toString(),
-      lng: location?.lng.toString(),
-      range: "5",
-    };
-    router.push(pathname + "?" + createQueryString(params));
+    if (location) {
+      pushToRouter({
+        lat: location.lat.toString(),
+        lng: location.lng.toString(),
+        range: "5",
+      });
+    }
   };
 
-  // 検索フォームのプロパティ
+  // 検索ボックスのprops
   const searchProps = {
     inputProps: {
       value: searchText,
       placeholder: "キーワード",
     },
     buttonProps: {
-      // ここに必要な ButtonProps を追加します
       className: "bg-green-700 hover:bg-green-900 text-white w-24",
     },
     icon: <AiOutlineSearch />,
@@ -83,12 +66,7 @@ export const SearchShop: FC = () => {
 
   return (
     <div className="flex flex-col lg:flex-nowrap justify-center pb-4 lg:justify-start">
-      {/* 検索バー */}
-      <InputWithButton {...searchProps} isButton={true}>
-        検索
-      </InputWithButton>
-
-      {/* 現在地で検索ボタン */}
+      <InputWithButton {...searchProps}>検索</InputWithButton>
       <Button
         onClick={handlePushLocation}
         className="rounded-md mt-4 bg-green-700 hover:bg-green-900 text-white container h-8 max-w-sm"
@@ -98,3 +76,5 @@ export const SearchShop: FC = () => {
     </div>
   );
 };
+
+export default SearchShop;
