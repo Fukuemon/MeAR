15164e637b715b3f885e0bbdb07c57f0e38aeb0e
Fukuemon <Fukuemony029@gmail.com>
2023-09-17 12:09:34 +0900

    💄 ProfilePostコンポーネントの作成 #1 #9
    投稿画像にいいねとコメントを表示させる
    これをmapで展開することで、投稿を表示させる

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import { PostNavbar } from "../components/elements/Navbar/Back";
 import Profile from "../components/Profile";
 import { mockProfileData } from "@/model/Profile";
+import { PostCardModel } from "@/model/PostCard";
 
 const profile = mockProfileData; // モックデータを取得
+const post = PostCardModel; // モックデータを取得
+
+const contents = {
+  profile,
+  post,
+};
 
 const MyProfilePage = () => {
   return (
     <div>
       <PostNavbar name={profile.nickName} />
-      <Profile profile={profile} />
+      <Profile {...contents} />
     </div>
   );
 };
