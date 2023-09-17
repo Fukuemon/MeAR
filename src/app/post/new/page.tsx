a9a36570f24619a5dbe7686b2ec1898d10a90340
Fukuemon <Fukuemony029@gmail.com>
2023-09-17 13:17:52 +0900

    💄 投稿作成画面のコンポーネントファイルの追加 #1 #12

--- src/app/post/new/page.tsx
+++ src/app/post/new/page.tsx
@@ -11,20 +11,25 @@ const NewPostPage = () => {
     <div>
       {/*店舗が選択されている場合はNavbarに店舗名を表示*/}
       <PostNavbar name={`店舗名：${selectedShop?.name}`} />
-      {/*店舗が選択されている場合は店舗情報を表示 */}
-      {selectedShop ? (
-        // 店舗名
-        <div>
-          <p className="text-lg py-2">店舗名: {selectedShop.name}</p>
-          {/* 店舗の住所 */}
-          <p className="text-lg py-2">住所: {selectedShop.address}</p>
-          <Link href={selectedShop.urls.pc} className="text-lg py-2">
-            公式サイト
-          </Link>
-        </div>
-      ) : (
-        <p className="text-lg py-2">No Shop Selected</p>
-      )}
+      {/* 日付選択 */}
+      <div className="p-8">
+        <p className="text-lg py-2">日付</p>
+        {/*店舗が選択されている場合は店舗情報を表示 */}
+        {selectedShop ? (
+          <div>
+            {/* 店舗の住所 */}
+            <p className="text-lg py-2">住所: {selectedShop.address}</p>
+            <Link
+              href={selectedShop.urls.pc}
+              className="text-lg py-2 text-blue-500"
+            >
+              公式サイト
+            </Link>
+          </div>
+        ) : (
+          <p className="text-lg py-2">No Shop Selected</p>
+        )}
+      </div>
     </div>
   );
 };
