3041d1facee358ea912e65d5cca6311842b7a2e4
Fukuemon <Fukuemony029@gmail.com>
2023-09-17 12:06:31 +0900

    🏷️ 🎨 propsとして受け取る値を変更 #1 #9
    Headerでは、profile情報を受け取り、ProfilePostコンポーネントで投稿のみを受け取るようにする

--- src/app/components/Profile/Header/index.tsx
+++ src/app/components/Profile/Header/index.tsx
@@ -3,28 +3,23 @@ import React, { FC } from "react";
 
 import { Profile } from "@/types/Profile/types";
 
-type Props = {
-  profile: Profile;
-  contents?: any; // ここにcontents(post, like, model)を追加
-};
-
-const ProfileHeader: FC<Props> = (props) => {
+const ProfileHeader: FC<Profile> = (profile) => {
   return (
-    <div className="max-w-6xl pl-2 py-4">
+    <div className="max-w-6xl pl-4 py-8">
       <div className="flex">
         {/* avatar画像 */}
         <div className="">
           <Image
-            src={props.profile.img}
+            src={profile.img}
             alt="steak"
             width={200}
             height={200}
-            className="rounded-full w-24 h-24"
+            className="rounded-full w-28 h-28 sm:w-40 md:h-40"
           />
         </div>
 
         {/* 投稿数・フォローフォロワー*/}
-        <div className="flex  pt-6  px-4">
+        <div className="flex justify-center pt-6  px-4">
           {/* 投稿数 */}
           <div className="flex flex-col items-center px-4">
             <h2 className="font-bold text-lg">1</h2>
@@ -32,16 +27,12 @@ const ProfileHeader: FC<Props> = (props) => {
           </div>
           {/* フォロー数 */}
           <div className="flex flex-col items-center px-4">
-            <h2 className="font-bold text-lg">
-              {props.profile.followings.length}
-            </h2>
+            <h2 className="font-bold text-lg">{profile.followings.length}</h2>
             <h2 className="font-thin text-xs text-gray-500">フォロー</h2>
           </div>
           {/* フォロワー数 */}
           <div className="flex flex-col items-center">
-            <h2 className="font-bold text-lg">
-              {props.profile.followers.length}
-            </h2>
+            <h2 className="font-bold text-lg">{profile.followers.length}</h2>
             <h2 className=" font-thin text-xs text-gray-500">フォロワー</h2>
           </div>
         </div>
