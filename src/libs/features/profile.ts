d7d21bb283d3d2b1fc69b0b497635f17b45f4a75
Fukuemon <Fukuemony029@gmail.com>
2023-09-17 09:33:32 +0900

    ✨ 💩 仮のapi取得ファイル作成#9

---
+++ src/libs/features/profile.ts
@@ -0,0 +1,5 @@
+export const getProfileList = async (userId: string) => {
+  const res = await fetch(`http://localhost:8080/api/profiles/${userId}`);
+  const data = await res.json();
+  return data;
+};
