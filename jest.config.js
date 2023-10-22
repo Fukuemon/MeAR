425f3abe853207315eedd50ecaab349b588eeaa2
Fukuemon <Fukuemony029@gmail.com>
2023-10-22 14:32:10 +0900

    🔧 jestの設定ファイルの編集 #19

--- jest.config.js
+++ jest.config.js
@@ -10,11 +10,9 @@ const createJestConfig = nextJest({
 const config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
-  preset: 'ts-jest'
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom'
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
-
 module.export = createJestConfig(config)
