68054a1d9094d316e95e592bd8fef3b01e5c7747
Fukuemon <Fukuemony029@gmail.com>
2023-11-02 22:09:54 +0900

    💄 style: #21 #29 投稿カードのヘッダーをpropsで情報を受け取るように修正

--- src/app/post/_components/CardItem/index.tsx
+++ src/app/post/_components/CardItem/index.tsx
@@ -1,31 +1,36 @@
 import React, { FC } from 'react'
+import clsx from 'clsx'
 import Image from 'next/legacy/image'
-import steak from '/public/steakcombo.jpeg'
 
 import { AiOutlineHeart } from 'react-icons/ai'
 import { BsShop } from 'react-icons/bs'
+import stake from '/public/steakcombo.jpeg'
 
-import { Author, tPostCard } from '@/types/Post/types'
+import { PostListItem } from '@/types/Post/types'
 import { CardHeader } from './Header'
 
-// Propsの型定義
 type Props = {
-  author: Author
-  post: tPostCard
-  isLike: boolean
+  id: string
+  post: PostListItem
 }
 
-const PostCard: FC<Props> = (props) => {
+const PostCardItem: FC<Props> = ({ id, post }) => {
+  const loggedInUserId = id
+
+  const isLikedByLoggedInUser = post.likes?.includes(loggedInUserId)
+
   return (
-    <div className="card">
+    <div className="m-2  w-screen overflow-hidden  rounded-lg  border bg-white text-gray-700 md:max-w-md">
       {/* ヘッダー */}
-      <CardHeader {...props} />
+      <CardHeader author={post.author} visited_date={post.visited_date} />
 
       {/* コンテンツ */}
       {/* 画像 */}
-      <Image src={steak} className="h-full w-full object-cover" alt="ステーキコンボ" />
+      {post.menu_photo && (
+        <Image src={stake} className="h-full w-full object-cover" alt="ステーキコンボ" width={500} height={300} />
+      )}
 
-      <div className="flex justify-between">
+      <div className="flex justify-between border">
         {/* 左側のコンテンツ */}
         {/* 店舗 */}
         <div className="flex flex-col gap-3 px-5 py-3">
@@ -34,29 +39,25 @@ const PostCard: FC<Props> = (props) => {
               <BsShop />
             </span>
             <h2 className="pl-1" title="ステーキコンボ">
-              {props.post.restaurant.name}
+              {post.restaurant.name}
             </h2>
           </div>
           {/* メニュー */}
           <h2 className="card-subtitle" title="ステーキコンボ">
-            {props.post.menu}
+            {post.menu_name}
           </h2>
         </div>
 
         {/* 右側のコンテンツ */}
         <div className="flex flex-col items-center gap-3 p-3 px-5">
           {/* 3Dタグ　：　モデルがあるかないかで表示を変える */}
-          {props.post.model ? <span className="badge">3D</span> : null}
+          {post.menu_model === '3D' ? <span className="badge">3D</span> : null}
           {/* いいねボタン：いいねの状態によって表示を変える */}
-          {props.isLike ? (
-            <AiOutlineHeart className="text-4xl text-red-500" />
-          ) : (
-            <AiOutlineHeart className="text-4xl" />
-          )}
+          <AiOutlineHeart className={clsx('text-4xl text-gray-100', { 'text-pink-600': isLikedByLoggedInUser })} />
         </div>
       </div>
     </div>
   )
 }
 
-export default PostCard
+export default PostCardItem
