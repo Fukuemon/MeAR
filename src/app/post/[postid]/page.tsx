51088e210f03b663d82e7c0ab6adab97a9d2f32c
Fukuemon <Fukuemony029@gmail.com>
2023-11-04 15:50:49 +0900

    🐛 fix: #29 投稿詳細を取得するapiをコメントアウトし、デプロイのテストを行う

--- src/app/post/[postid]/page.tsx
+++ src/app/post/[postid]/page.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
-import { notFound } from 'next/navigation'
-import { PostDetailType } from '@/types/Post/types'
-import { getPostDetail } from '../lib/getPostDetails'
+// import { notFound } from 'next/navigation'
+// import { PostDetailType } from '@/types/Post/types'
+// import { getPostDetail } from '../lib/getPostDetails'
 import { getPostList } from '../lib/getPostList'
-import PostDetail from './_components/DetailPost'
+// import PostDetail from './_components/DetailPost'
 
 export const generateStaticParams = async () => {
   const posts = await getPostList()
 
   return posts.map((post) => ({
-    postid: post.id.toString()
+    postId: post.id.toString()
   }))
 }
 
-export default async function PostDetailPage({ params }: { params: { postid: string } }) {
-  const { postid } = params
-  console.log(postid)
-  const post: PostDetailType = await getPostDetail(postid)
+export default async function PostDetailPage({ params }: { params: { postId: string } }) {
+  const { postId } = params
+  console.log(postId)
+  // const post: PostDetailType = await getPostDetail(postid)
 
-  if (!post) {
-    notFound()
-  }
+  // if (!post) {
+  //   notFound()
+  // }
 
-  return <PostDetail props={post} />
+  // return <PostDetail props={post} />
+  return <div>test</div>
 }
