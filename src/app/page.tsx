ae924fb904cb83492f71cad08f315a930198b3a3
Fukuemon <Fukuemony029@gmail.com>
2023-11-04 13:53:05 +0900

    ✨ feat: #29 各投稿のidに合わせて詳細ページへのリンクを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { cookies } from 'next/headers'
-import Link from 'next/link'
 import { PostList } from '@/types/Post/types'
 import Navbar from './_components/Common/Navbar/NavigationBar'
 import { PostCardList } from './post/_components/CardList/PostCardList'
@@ -14,9 +13,7 @@ export default async function Home() {
     <div>
       <Navbar isLogin={isLogin} />
       <main className="flex min-h-screen items-center justify-center p-16">
-        <Link href="/post">
-          <PostCardList postList={postList} />
-        </Link>
+        <PostCardList postList={postList} />
       </main>
     </div>
   )
--- src/app/post/_components/CardList/PostCardList.tsx
+++ src/app/post/_components/CardList/PostCardList.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { PostList, PostListItem } from '@/types/Post/types'
 import PostCardItem from '../CardItem'
 
@@ -10,7 +11,11 @@ export function PostCardList({ postList }: PostListProps) {
   return (
     <div className="flex max-w-2xl flex-col  md:items-center md:justify-center">
       {postList.map((post: PostListItem) => {
-        return <PostCardItem key={post.id} id="1" post={post} />
+        return (
+          <Link key={post.id} href={`/post/${post.id}`}>
+            <PostCardItem key={post.id} id="1" post={post} />
+          </Link>
+        )
       })}
     </div>
   )
