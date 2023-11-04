e3790daf3eeaa309f7e7dc4a5dc6facdec6befd6
Fukuemon <Fukuemony029@gmail.com>
2023-11-04 14:38:22 +0900

    ✨ feat: #29 投稿詳細のページ作成
    generateStaticParamsを使って、idに紐づく投稿詳細画面を作成

--- src/app/post/[postid]/page.tsx
+++ src/app/post/[postid]/page.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { notFound } from 'next/navigation'
+import PostDetail from '@/app/post/[postId]/_components/DetailPost'
+import { PostDetailType } from '@/types/Post/types'
+import { getPostDetail } from '../lib/getPostDetails'
+import { getPostList } from '../lib/getPostList'
 
-const PostDetailPage = () => {
-  return <div>page</div>
+export const generateStaticParams = async () => {
+  const posts = await getPostList()
+
+  return posts.map((post) => ({
+    postId: post.id.toString()
+  }))
 }
 
-export default PostDetailPage
+export default async function PostDetailPage({ params }: { params: { postId: string } }) {
+  const { postId } = params
+  console.log(postId)
+  const post: PostDetailType = await getPostDetail(postId)
+
+  if (!post) {
+    notFound()
+  }
+
+  return <PostDetail props={post} />
+}
