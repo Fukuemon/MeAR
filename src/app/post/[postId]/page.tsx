3d46c88cc9a04e7a09556c8fb15c10016ef5cf82
Fukuemon <Fukuemony029@gmail.com>
2023-11-04 17:15:52 +0900

    ⚡️perf: #29 データ取得をaxiosを使った方法に変更

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { notFound } from 'next/navigation'
-import { PostDetailType } from '@/types/Post/types'
 import { getPostDetail } from '../lib/getPostDetails'
 import { getPostList } from '../lib/getPostList'
 import PostDetail from './_components/PostDetail/PostDetail'
@@ -16,7 +15,8 @@ export const generateStaticParams = async () => {
 export default async function PostDetailPage({ params }: { params: { postId: string } }) {
   const { postId } = params
   console.log(postId)
-  const post: PostDetailType = await getPostDetail(postId)
+
+  const post = await getPostDetail(postId)
 
   if (!post) {
     notFound()
--- src/app/post/lib/getPostDetails.ts
+++ src/app/post/lib/getPostDetails.ts
@@ -1,17 +1,13 @@
+import { AxiosError } from 'axios'
+import { handleApiError } from '@/libs/axios/handleError'
+import { api } from '@/libs/axios/instance'
 import { PostDetailType } from '@/types/Post/types'
 
-export const getPostDetail = async (id: string): Promise<PostDetailType> => {
-  const url = process.env.NEXT_PUBLIC_BACKEND_URL + `/post/${id}/`
-  const res = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch post detail')
+export const getPostDetail = async (id: string) => {
+  try {
+    const post = await api.get<PostDetailType>(`/post/${id}/`)
+    return post.data
+  } catch (error) {
+    handleApiError(error as AxiosError, 'Failed to fetch post detail')
   }
-
-  return res.json() as Promise<PostDetailType>
 }
