0dd7df0058d8b9bce45f26cf60ced8dc507680b8
Fukuemon <Fukuemony029@gmail.com>
2023-11-04 17:50:09 +0900

    🐛 fix: #29 コードを復元

---
+++ src/app/post/lib/getPostDetail.ts
@@ -0,0 +1,13 @@
+import { AxiosError } from 'axios'
+import { handleApiError } from '@/libs/axios/handleError'
+import { api } from '@/libs/axios/instance'
+import { PostDetailType } from '@/types/Post/types'
+
+export const getPostDetail = async (id: string) => {
+  try {
+    const post = await api.get<PostDetailType>(`/post/${id}/`)
+    return post.data
+  } catch (error) {
+    handleApiError(error as AxiosError, 'Failed to fetch post detail')
+  }
+}
