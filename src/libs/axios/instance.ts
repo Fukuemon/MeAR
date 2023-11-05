9c8254e69cb5ccce824f4b16687f4fe11d8f8ab9
Fukuemon <Fukuemony029@gmail.com>
2023-11-05 15:41:41 +0900

    🐛 fix: #29 axiosインスタンスにheaderとしてjson形式を指定する

--- src/libs/axios/instance.ts
+++ src/libs/axios/instance.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
 export const fetcher = (url: string) => api.get(url).then((res) => res.data)
