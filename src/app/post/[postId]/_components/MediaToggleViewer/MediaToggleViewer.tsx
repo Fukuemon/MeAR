be6cd25503f327f372859d72bfec3c39a5e64dca
Fukuemon <Fukuemony029@gmail.com>
2023-11-13 10:05:12 +0900

    ♻️ refactor: #29 #27 投稿詳細のコンポーネントを細分化

---
+++ src/app/post/[postId]/_components/MediaToggleViewer/MediaToggleViewer.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { Heart } from 'lucide-react'
+import Image from 'next/image'
+import { DynamicModelViewer } from '@/app/post/_components/ModelViewer/DynamicModelViewer'
+
+type Props = {
+  menuPhoto: string
+  menuModel?: string | null
+  isModel: boolean
+}
+
+export default function ImageOrModelViewer({ menuPhoto, menuModel, isModel }: Props) {
+  const [isLike, setIsLike] = useState(false)
+  // いいねボタンを押したときの処理
+  const onClickLike = () => {
+    setIsLike((preveState) => !preveState)
+  }
+  return (
+    <div className="relative z-0">
+      {isModel && menuModel ? (
+        <div className="flex items-center justify-center">
+          <DynamicModelViewer src={menuModel} />
+        </div>
+      ) : (
+        <Image src={menuPhoto} width={500} height={400} className="h-full w-full object-cover" alt="" />
+      )}
+
+      <span className="absolute right-4 top-2">
+        <Heart
+          onClick={onClickLike}
+          className={`h-10 w-10 fill-current ${isLike ? 'text-primary' : 'text-gray-400'}`}
+        />
+      </span>
+    </div>
+  )
+}
