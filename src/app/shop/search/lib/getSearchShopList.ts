365357a6ea7231b9b68fdb6688a4dc3a51c34b3d
Fukuemon <Fukuemony029@gmail.com>
2023-11-13 21:47:20 +0900

    ♻️ refactor: #36 #27 ロジックを切り出し、助長なコードを削除

---
+++ src/app/shop/search/lib/getSearchShopList.ts
@@ -0,0 +1,21 @@
+import { Shop } from '../../atom'
+
+export type SearchParams = {
+  keyword?: string
+  range?: number
+  lat?: number
+  lng?: number
+}
+
+const defaultEndpoint = `${process.env.NEXT_PUBLIC_HOTPEPPER_API}?key=${process.env.NEXT_PUBLIC_HOTPEPPER_API_KEY}&format=json&count=10`
+// getShopList.ts
+export async function getShopList(searchParams: SearchParams): Promise<Shop[]> {
+  const apiParams = new URLSearchParams()
+  Object.entries(searchParams).forEach(([key, value]) => {
+    if (value !== undefined) apiParams.append(key, value.toString())
+  })
+  console.log(`${defaultEndpoint}&${apiParams.toString()}`)
+  const response = await fetch(`${defaultEndpoint}&${apiParams.toString()}`)
+  const data = await response.json()
+  return data.results.shop
+}
--- src/app/shop/search/page.tsx
+++ src/app/shop/search/page.tsx
@@ -1,54 +1,23 @@
+// SearchShopPage.tsx
+import { Suspense } from 'react'
 import { BackNavbar } from '@/app/_components/Common/Navbar/BackNavigationBar'
-import { SearchShop } from '@/app/shop/search/_components/Search'
-import { Shop } from '../atom'
+import ShopSearchInput from './_components/Search/ShopSearchInput'
 import ShopList from './_components/ShopList'
+import { SearchParams, getShopList } from './lib/getSearchShopList'
 
-interface SearchParams {
-  keyword?: string
-  range?: number
-  lat?: number
-  lng?: number
-}
-
-const defaultEndpoint = `${process.env.NEXT_PUBLIC_HOTPEPPER_API}?key=${process.env.NEXT_PUBLIC_HOTPEPPER_API_KEY}&format=json&count=10`
-
-export default async function SearchShopPage({
-  searchParams
-}: {
-  searchParams: SearchParams // クエリパラメータ(?keyword=〇〇&lat=〇〇&lng=〇〇&range=〇〇)を受け取る
-}) {
-  const { keyword, lat, lng, range } = searchParams // クエリパラメータを分割代入
-
-  const apiParams = new URLSearchParams() // URLSearchParamsをインスタンス化 (クエリパラメータを作成するためのクラス)
-
-  // クエリパラメータを作成する関数
-  const appendParam = (key: string, value: number | string | undefined) => {
-    if (value !== undefined) apiParams.append(key, value.toString()) // valueがundefinedでなければクエリパラメータを追加
-  }
-
-  appendParam('keyword', keyword)
-  appendParam('lat', lat)
-  appendParam('lng', lng)
-  appendParam('range', range)
-
-  // APIを叩く
-  try {
-    const res = await fetch(`${defaultEndpoint}&${apiParams.toString()}`)
-    const data = await res.json()
-    const shops = data.results.shop
-
-    return (
-      <div>
-        <BackNavbar name="店舗検索" />
-        <main className="flex flex-col items-center justify-center p-8">
-          <SearchShop />
-          <ShopList shops={shops as Shop[]} />
-        </main>
-      </div>
-    )
-  } catch (error: unknown) {
-    // エラー処理
-    const errorMessage = error instanceof Error ? error.message : 'An error occurred'
-    return <div>Error fetching data: {errorMessage}</div>
-  }
+export default async function SearchShopPage({ searchParams }: { searchParams: SearchParams }) {
+  console.log(searchParams)
+  const shops = await getShopList(searchParams)
+  console.log(shops)
+  return (
+    <div>
+      <BackNavbar name="店舗検索" />
+      <main className="flex flex-col items-center justify-center p-8">
+        <ShopSearchInput />
+        <Suspense fallback={<div>loading...</div>}>
+          <ShopList shops={shops} />
+        </Suspense>
+      </main>
+    </div>
+  )
 }
