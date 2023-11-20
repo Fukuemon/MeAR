d3f882607b88462675cfe68b0a69db64a342c4ad
Fukuemon <Fukuemony029@gmail.com>
2023-11-20 10:20:38 +0900

    ✨ feat: #36 投稿フォームのコンポーネントにロジックを追加

--- src/app/post/create/_components/CreatePost/index.tsx
+++ src/app/post/create/_components/CreatePost/index.tsx
@@ -1,19 +1,39 @@
 'use client'
+import React, { ChangeEvent, Suspense, useState } from 'react'
 import format from 'date-fns/format'
 import MediaUpload from '@/app/_components/Form/MediaUpload/MediaUpload'
-import StarRating from '@/app/_components/Form/StarRating/StarRating'
+import { StarRating } from '@/app/_components/Form/StarRating/StarRating'
+import Loading from '@/app/loading'
+import Tag from '@/app/post/_components/Tag/Tag'
+import { useGetTags } from '@/app/post/hooks/useGetTags'
 import { Button } from '@/components/ui/button'
-import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Textarea } from '@/components/ui/textarea'
+import { TagType } from '@/types/Post/types'
 import { DatePicker } from '../../../../_components/Form/DatePicker'
 import InputWithIcon from '../../../../_components/Form/InputWithIcon'
+import { TagSelectModal } from '../TagSelectModal/TagSelectModal'
 import { useCreatePostForm } from './CreatePost.hooks'
 
 const CreatePost = () => {
-  // メニューボックスのprops
+  const [selectedTags, setSelectedTags] = useState<TagType[]>([]) // 選択されたタグ
+  const [imagePreview, setImagePreview] = useState<string>('') // 画像プレビュー
 
-  const { form, onSubmit } = useCreatePostForm()
+  const { tags } = useGetTags() // タグ一覧取得
+  const { form, onSubmit, loading, setImageSrc, setModelSrc } = useCreatePostForm()
 
+  const watchImage = form.watch('menu_photo')
+  const watchModel = form.watch('menu_model')
+  const selectTag = (tag: TagType[]) => {
+    setSelectedTags(tag)
+    // タグのidのみを取り出してセットする
+    form.setValue(
+      'tags',
+      tag.map((t) => t.id)
+    )
+  }
+
+  if (loading) return <Loading />
   return (
     <div>
       <Form {...form}>
@@ -37,57 +57,106 @@ const CreatePost = () => {
             </Button>
           </div>
 
+          {/* 画像 */}
           <div className="flex items-center justify-center space-x-14 py-8">
-            <FormField
-              control={form.control}
-              name="menu_photo"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <MediaUpload fileType="image/*" icon="Image" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="menu_model"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <MediaUpload fileType=".glb" icon="FileBox" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            <div className="flex flex-col items-center justify-center gap-y-1">
+              <FormField
+                control={form.control}
+                name="menu_photo"
+                render={({}) => (
+                  <FormItem>
+                    <FormControl>
+                      <MediaUpload
+                        image={imagePreview}
+                        fileType="image/*"
+                        icon="Image"
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                          const file: File | undefined = e.target.files ? e.target.files[0] : undefined
+                          if (file) {
+                            setImageSrc(file)
+                            setImagePreview(URL.createObjectURL(file))
+                          }
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* 写真が入力されているかで表示を変える */}
+              <FormDescription>{watchImage ? '挿入済み' : '画像を追加'}</FormDescription>
+            </div>
+
+            {/* 3Dファイル */}
+            <div className="flex flex-col items-center justify-center gap-y-1">
+              <FormField
+                control={form.control}
+                name="menu_model"
+                render={({}) => (
+                  <FormItem>
+                    <FormControl>
+                      <MediaUpload
+                        fileType=".glb"
+                        icon="FileBox"
+                        onChange={(e) => {
+                          const file: File | undefined = e.target.files ? e.target.files[0] : undefined
+                          if (file) {
+                            setModelSrc(file)
+                          }
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* 3Dファイルが入力されているかで表示を変える */}
+              <FormDescription>{watchModel ? '挿入済み' : '3Dファイルを追加'}</FormDescription>
+            </div>
           </div>
+
           <div className="flex flex-col gap-y-6 py-4">
+            {/* メニュー名 */}
             <FormField
               control={form.control}
               name="menu_name"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <InputWithIcon {...field} icon="Utensils" placeholder="メニューを追加" className="w-full" />
+                    <InputWithIcon
+                      type="text"
+                      {...field}
+                      icon="Utensils"
+                      placeholder="メニューを追加"
+                      className="w-full"
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
+
+            {/* 値段 */}
             <FormField
               control={form.control}
               name="price"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <InputWithIcon {...field} icon="JapaneseYen" placeholder="値段を追加" className="w-full" />
+                    <InputWithIcon
+                      type="number"
+                      {...field}
+                      icon="JapaneseYen"
+                      placeholder="値段を追加"
+                      className="w-full"
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
+
+            {/* 評価 */}
             <FormField
               control={form.control}
               name="score"
@@ -100,6 +169,8 @@ const CreatePost = () => {
                 </FormItem>
               )}
             />
+
+            {/* レビュー */}
             <FormField
               control={form.control}
               name="review_text"
@@ -115,11 +186,16 @@ const CreatePost = () => {
           </div>
         </form>
       </Form>
-
-      {/* 評価 */}
-
       {/* tag */}
-      <Button className="bg-green-500 text-white hover:bg-green-900"># タグを追加</Button>
+      <Suspense fallback={<Loading />}>
+        {/* タグ一覧表示 */}
+        {Array.isArray(tags) && <TagSelectModal tags={tags} onSelect={selectTag} />}
+      </Suspense>
+
+      {/* タグ表示 */}
+      <div className="my-4  flex flex-wrap gap-2">
+        {selectedTags && selectedTags.map((tag) => <Tag key={tag.id} name={tag.tag} />)}
+      </div>
     </div>
   )
 }
