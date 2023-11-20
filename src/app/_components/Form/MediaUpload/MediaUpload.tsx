ac3219b5915f162c378832d1c3c34e0a401fd9ed
Fukuemon <Fukuemony029@gmail.com>
2023-11-20 10:12:43 +0900

    ✨feat: #36 画像入力の有無によってPreview機能を実装

--- src/app/_components/Form/MediaUpload/MediaUpload.tsx
+++ src/app/_components/Form/MediaUpload/MediaUpload.tsx
@@ -1,22 +1,36 @@
+'use client'
 import React from 'react'
 import { icons } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 
 type Props = {
+  image?: string
   fileType: string
   icon: keyof typeof icons
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function MediaUpload({ fileType, icon }: Props) {
+// React.forwardRefを使用してコンポーネントをラップ
+const MediaUpload = React.forwardRef<HTMLInputElement, Props>(({ fileType, icon, onChange, image }, ref) => {
   const LucideIcon = icons[icon]
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <label className="flex h-32 w-32 cursor-pointer flex-col items-center rounded-lg border bg-gray-50 px-4 py-6 uppercase tracking-wide text-blue-500 shadow-lg hover:bg-gray-300">
-        <LucideIcon className="" />
-        <Input type="file" className="hidden" accept={fileType} />
-      </label>
-      <p className="pt-2 text-sm text-gray-500">{fileType === 'image/*' ? '画像を追加' : '3Dファイルを追加'}</p>
+      {
+        /* 画像が入力されているかで表示を変える */
+        image ? (
+          <img src={image} alt="image" className="h-32 w-32" />
+        ) : (
+          <label className="flex h-32 w-32 cursor-pointer flex-col items-center rounded-lg border bg-gray-50 px-4 py-6 uppercase tracking-wide text-blue-500 shadow-lg hover:bg-gray-300">
+            <LucideIcon />
+            <Input type="file" className="hidden" accept={fileType} ref={ref} onChange={onChange} />
+          </label>
+        )
+      }
     </div>
   )
-}
+})
+
+MediaUpload.displayName = 'MediaUpload'
+
+export default MediaUpload
