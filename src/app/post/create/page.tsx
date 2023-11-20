4cb76a3e9f19c9e6897e263f759a7e6000e55ad4
Fukuemon <Fukuemony029@gmail.com>
2023-11-20 10:25:58 +0900

    ✨ feat: #36 投稿作成ページで店舗情報を取得

--- src/app/post/create/page.tsx
+++ src/app/post/create/page.tsx
@@ -1,23 +1,33 @@
 'use client'
+import { Suspense } from 'react'
 import { useAtom } from 'jotai'
+import { useRouter } from 'next/navigation'
 import { BackNavbar } from '@/app/_components/Common/Navbar/BackNavigationBar'
+import Loading from '@/app/loading'
 import CreatePost from '@/app/post/create/_components/CreatePost'
 import { selectedShopAtom } from '@/app/shop/atom'
 import ShopInfo from '../_components/CardItem/ShopInfo/ShopInfo'
 
 const NewPostPage = () => {
+  const router = useRouter()
   const [selectedShop] = useAtom(selectedShopAtom) // 選択された店舗情報を取得
+  if (!selectedShop) return router.back()
+  const { name, address, lat, lng, large_area, urls } = selectedShop
+  const url = urls.pc
+  const restaurant = { name, address, lat, lng, area: large_area.name, url }
 
   return (
-    <div>
+    <div className="pb-20">
       {/*店舗が選択されている場合はNavbarに店舗名を表示*/}
       <BackNavbar name="投稿作成" />
       {/* 日付選択 */}
       <div className="p-8">
-        <CreatePost />
+        <Suspense fallback={<Loading />}>
+          <CreatePost />
+        </Suspense>
 
         {/*店舗が選択されている場合は店舗情報を表示 */}
-        {selectedShop && <ShopInfo restaurant={selectedShop} />}
+        {selectedShop && <ShopInfo restaurant={restaurant} isDetail />}
       </div>
     </div>
   )
