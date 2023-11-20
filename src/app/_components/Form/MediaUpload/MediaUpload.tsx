c2b63091b44301d1c3fe7ce5a346a3efebaae42f
Fukuemon <Fukuemony029@gmail.com>
2023-11-20 13:02:19 +0900

    🐛 fix: #36 labelで囲み、入力後に再度入力できない問題を改善

--- src/app/_components/Form/MediaUpload/MediaUpload.tsx
+++ src/app/_components/Form/MediaUpload/MediaUpload.tsx
@@ -1,17 +1,19 @@
 'use client'
 import React from 'react'
+import clsx from 'clsx'
 import { icons } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 
 type Props = {
   image?: string
+  model?: File
   fileType: string
   icon: keyof typeof icons
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 // React.forwardRefを使用してコンポーネントをラップ
-const MediaUpload = React.forwardRef<HTMLInputElement, Props>(({ fileType, icon, onChange, image }, ref) => {
+const MediaUpload = React.forwardRef<HTMLInputElement, Props>(({ fileType, icon, onChange, image, model }, ref) => {
   const LucideIcon = icons[icon]
 
   return (
@@ -19,10 +21,20 @@ const MediaUpload = React.forwardRef<HTMLInputElement, Props>(({ fileType, icon,
       {
         /* 画像が入力されているかで表示を変える */
         image ? (
-          <img src={image} alt="image" className="h-32 w-32" />
+          <label className="h-32 w-32 cursor-pointer">
+            <img src={image} alt={image} className=" object-container h-32 w-32" />
+            <div>
+              <Input type="file" className="hidden" accept={fileType} ref={ref} onChange={onChange} />
+            </div>
+          </label>
         ) : (
-          <label className="flex h-32 w-32 cursor-pointer flex-col items-center rounded-lg border bg-gray-50 px-4 py-6 uppercase tracking-wide text-blue-500 shadow-lg hover:bg-gray-300">
-            <LucideIcon />
+          <label
+            className={clsx(
+              'flex h-32 w-32 cursor-pointer flex-col items-center rounded-lg border bg-gray-50 px-4 py-6 uppercase tracking-wide text-blue-500 shadow-lg hover:bg-gray-300',
+              { 'bg-white': model }
+            )}
+          >
+            <LucideIcon className={model && 'text-primary'} />
             <Input type="file" className="hidden" accept={fileType} ref={ref} onChange={onChange} />
           </label>
         )
