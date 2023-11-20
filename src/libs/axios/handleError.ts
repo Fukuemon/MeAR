b102e986e7f2b7e7d67bad14b129e516bf658fe9
Fukuemon <Fukuemony029@gmail.com>
2023-11-20 10:19:13 +0900

    ✨ feat: #36 shadcnからtoastの追加

--- src/libs/axios/handleError.ts
+++ src/libs/axios/handleError.ts
@@ -1,13 +1,34 @@
 import { AxiosError } from 'axios'
+import { toast } from '@/components/ui/use-toast'
 
 export const handleApiError = (error: AxiosError, errorMessage: string) => {
   if (error.response) {
+    toast({
+      title: errorMessage,
+      variant: 'destructive'
+      // action: <ToastAction altText="Try again">Try again</ToastAction>
+    })
     console.error(errorMessage, error.response.data)
     console.error('Status code:', error.response.status)
     console.error('Headers:', error.response.headers)
   } else if (error.request) {
+    toast({
+      title: errorMessage,
+      variant: 'destructive'
+      // action: <ToastAction altText="Try again">Try again</ToastAction>
+    })
     console.error(errorMessage, error.request)
   } else {
+    toast({
+      title: errorMessage,
+      variant: 'destructive'
+      // action: <ToastAction altText="Try again">Try again</ToastAction>
+    })
     console.error('Error message:', error.message)
+    toast({
+      title: errorMessage,
+      variant: 'destructive'
+      // action: <ToastAction altText="Try again">Try again</ToastAction>
+    })
   }
 }
