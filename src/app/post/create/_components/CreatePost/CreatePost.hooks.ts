b0e4f06f5749b3a5f04868b812b3dfe301b280d1
Fukuemon <Fukuemony029@gmail.com>
2023-11-23 08:05:18 +0900

    ♻️ refactor: #36 #27 処理を分割して関数にまとめる

--- src/app/post/create/_components/CreatePost/CreatePost.hooks.ts
+++ src/app/post/create/_components/CreatePost/CreatePost.hooks.ts
@@ -6,7 +6,7 @@ import { useAtom } from 'jotai'
 import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { Shop, selectedShopAtom } from '@/app/shop/atom'
+import { SelectedShop, selectedShopAtom } from '@/app/shop/atom'
 import { toast } from '@/components/ui/use-toast'
 import { handleApiError } from '@/libs/axios/handleError'
 import { api } from '@/libs/axios/instance'
@@ -21,7 +21,7 @@ const RestaurantSchema = z.object({
 })
 
 const PostCreateSchema = z.object({
-  restaurant: RestaurantSchema,
+  restaurant_data: RestaurantSchema,
   tags: z.array(z.number()),
   menu_name: z.string(),
   score: z.number().min(1).max(5),
@@ -51,17 +51,18 @@ export const useCreatePostForm = () => {
   const [selectedShop] = useAtom(selectedShopAtom)
   if (!selectedShop) {
     router.back()
-    throw new Error('No shop selected')
+    toast({ title: '店舗が選択されていません' })
+    throw new Error('店舗が選択されていません')
   }
-  const { id, name, address, lat, lng }: Shop = selectedShop
+  const { name, address, lat, lng }: SelectedShop = selectedShop
   const area = selectedShop.large_area.name
   const url = selectedShop.urls.pc
-  const restaurant = { id, name, address, area, lat, lng, url }
+  const restaurant = { name, address, area, lat, lng, url }
   console.log(restaurant)
   const form = useForm<PostCreate>({
     resolver: zodResolver(PostCreateSchema),
     defaultValues: {
-      restaurant: restaurant,
+      restaurant_data: restaurant,
       tags: [],
       menu_name: '',
       score: 3,
@@ -73,38 +74,85 @@ export const useCreatePostForm = () => {
     }
   })
 
-  const appendCreatePostFormData = async (formData: FormData, data: PostCreate) => {
-    formData.append('restaurant', JSON.stringify(data.restaurant))
-    formData.append('tags', JSON.stringify(data.tags))
-    formData.append('menu_name', data.menu_name)
-    formData.append('score', data.score.toString())
-    formData.append('price', data.price.toString())
-    formData.append('review_text', data.review_text)
-    formData.append('visited_date', data.visited_date)
-  }
-
   const onSubmit = async (data: PostCreate) => {
     setLoading(true)
     try {
-      const formData = new FormData()
-      await appendCreatePostFormData(formData, data)
-      if (imageSrc) {
-        formData.append('menu_photo', imageSrc)
-      }
-      if (modelSrc) {
-        formData.append('menu_model', modelSrc)
-      }
-      console.log(formData.getAll('menu_photo'))
-      console.log(formData.getAll('menu_model'))
-      await api.post('post/', formData, {
-        headers: { 'Content-Type': 'multipart/form-data', Authorization: `JWT ${accessToken}` }
-      })
-      router.push('/')
-      toast({ title: '投稿しました' })
+      console.log(data)
+      const formData = createFormData(data)
+      await postFormData(formData)
+      onSuccess()
     } catch (error) {
-      handleApiError(error as AxiosError, '投稿に失敗しました')
+      onError(error as AxiosError)
+    } finally {
+      setLoading(false)
     }
-    setLoading(false)
+  }
+
+  interface CustomFormData extends FormData {
+    append<T extends string | Blob | number>(name: `${string}[]` | keyof PostCreate, value: T, fileName?: string): void
+  }
+
+  const createFormData = (data: PostCreate): FormData => {
+    console.log(data)
+    const formData = new FormData() as CustomFormData
+    Object.entries(data).forEach(([key, value]) => {
+      appendFormData(formData, key as keyof PostCreate, value)
+    })
+
+    validateFormData(formData, 'menu_photo')
+    validateFormData(formData, 'menu_model')
+    formData.append('menu_photo', imageSrc as Blob)
+    formData.append('menu_model', modelSrc as Blob)
+
+    return formData
+  }
+
+  type FormDataValue =
+    | string
+    | number
+    | { name: string; address: string; lat: number; lng: number; area: string; url: string }
+    | number[]
+    | File
+    | null
+
+  const appendFormData = (formData: CustomFormData, key: keyof PostCreate, value: FormDataValue) => {
+    if (value === null) {
+      return
+    }
+    if (Array.isArray(value)) {
+      value.forEach((v) => formData.append(`${key}[]`, v))
+    } else if (typeof value === 'object' && value !== null) {
+      formData.append(key, JSON.stringify(value))
+    } else {
+      formData.append(key, value)
+    }
+  }
+
+  const validateFormData = (formData: FormData, key: string) => {
+    const value = formData.get(key)
+    if (value === 'null' || value === null) {
+      formData.delete(key)
+    }
+  }
+
+  const postFormData = async (formData: FormData) => {
+    await api.post('post/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `JWT ${accessToken}`
+      }
+    })
+  }
+
+  const onSuccess = () => {
+    router.push('/')
+    toast({ title: '投稿しました' })
+    setImageSrc(undefined)
+    setModelSrc(undefined)
+  }
+
+  const onError = (error: AxiosError) => {
+    handleApiError(error, '投稿に失敗しました')
   }
 
   return { form, onSubmit, loading, imageSrc, setImageSrc, modelSrc, setModelSrc }
