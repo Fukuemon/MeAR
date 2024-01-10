08bf1081eaf13b9dd3855adb5dbae7e73e0bec9e
Fukuemon <Fukuemony029@gmail.com>
2024-01-10 09:52:10 +0900

    ✨ feat: #49 画像圧縮のhooks追加

---
+++ src/app/post/lib/compressImage.ts
@@ -0,0 +1,42 @@
+import loadImage, { LoadImageOptions } from 'blueimp-load-image'
+
+type LoadImage = (
+  file: File | Blob | string,
+  callback: (canvas: HTMLCanvasElement) => void,
+  options?: LoadImageOptions
+) => void
+
+// 画像圧縮処理
+
+export const useCompressImage = () => {
+  const compressImage = (file: File): Promise<Blob> => {
+    return new Promise((resolve, reject) => {
+      ;(loadImage as LoadImage)(
+        file,
+        (canvas: HTMLCanvasElement) => {
+          if (!canvas.toBlob) {
+            reject(new Error('お使いのブラウザは Canvas.toBlob メソッドをサポートしていません'))
+            return
+          }
+
+          canvas.toBlob(
+            (blob) => {
+              if (blob) {
+                resolve(blob)
+              } else {
+                reject(new Error('Image compression resulted in null Blob'))
+              }
+            },
+            'image/jpeg',
+            0.7
+          )
+        },
+        {
+          maxWidth: 1200,
+          canvas: true
+        }
+      )
+    })
+  }
+  return { compressImage }
+}
