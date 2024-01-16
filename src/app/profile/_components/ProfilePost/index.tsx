3bd5521ffb8a0bba7ae356179ff0904dc131adf1
Fukuemon <Fukuemony029@gmail.com>
2024-01-16 09:38:54 +0900

    ⚡️ perf: #27 imageコンポーネントの修正

--- src/app/profile/_components/ProfilePost/index.tsx
+++ src/app/profile/_components/ProfilePost/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Heart } from 'lucide-react'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
 import { PostListItem } from '@/types/Post/types'
 
 const ProfilePost = (post: PostListItem) => {
   return (
     <Link href={`/post/${post.id}`} className="group relative">
-      <Image src={post.menu_photo} alt="post" width={200} height={200} className="h-28 w-28 rounded-lg object-cover" />
+      <img src={post.menu_photo} alt="post" width={200} height={200} className="h-28 w-28 rounded-lg object-cover" />
       {/* いいね、コメント */}
       {post.likes && post.likes.length > 0 ? (
         <div className="absolute left-1 top-1 z-10 h-full">
