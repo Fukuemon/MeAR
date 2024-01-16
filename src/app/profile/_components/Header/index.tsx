d26282b66660a950cd7bbf8948030ce38d0cc1ee
Fukuemon <Fukuemony029@gmail.com>
2024-01-16 11:33:05 +0900

    💄 style: #21 アバター画像の大きさを調整

--- src/app/profile/_components/Header/index.tsx
+++ src/app/profile/_components/Header/index.tsx
@@ -8,17 +8,16 @@ type ProfileProps = {
 
 const ProfileHeader: FC<ProfileProps> = ({ profile, post_count }) => {
   return (
-    <div className="w-96 py-8 pl-4">
-      <div className="flex items-center">
+    <div className="w-[400px] py-8 pl-4">
+      <div className="flex items-center space-x-4">
         {/* avatar画像 */}
-
-        <img
-          src={profile.img ? profile.img : '/user.png'}
-          alt="steak"
-          width={200}
-          height={200}
-          className="h-28 w-28 rounded-full sm:w-40 md:h-40"
-        />
+        <div className="sm: m-2 h-[100px] w-[100px] rounded-full border">
+          <img
+            src={profile.img ? profile.img : '/user.png'}
+            alt="steak"
+            className="h-full w-full rounded-full object-cover"
+          />
+        </div>
 
         {/* 投稿数・フォローフォロワー*/}
         <div className="flex justify-center   pt-6">
