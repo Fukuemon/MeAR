ad9d9d1470a0a19b2b51c64bc48419321e426529
Fukuemon <Fukuemony029@gmail.com>
2024-01-21 10:52:36 +0900

    🎨 art: #27 不必要なawait削除

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,22 @@ import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 import { getRefreshAccessToken } from './app/(auth)/lib/getRefreshAccessToken'
 import { verifyAccessToken } from './app/(auth)/lib/verifyAccessToken'
+import { toast } from './components/ui/use-toast'
 
 async function handleToken(request: NextRequest, accessToken: string | undefined, refreshToken: string | undefined) {
   // accessTokenがない場合は、refreshTokenを使って新しいaccessTokenを取得する
   if (!accessToken && refreshToken) {
     const tokens = await getRefreshAccessToken(refreshToken)
-    const newAccessToken = await tokens.access
-    const newRefreshToken = await tokens.refresh
+    const newAccessToken = tokens.access
+    const newRefreshToken = tokens.refresh
 
     if (newAccessToken && newRefreshToken) {
-      const response = await NextResponse.next()
-      await response.cookies.set('access', newAccessToken, { maxAge: 60 * 45 })
-      await response.cookies.set('refresh', newRefreshToken, { maxAge: 60 * 60 * 24 * 30 })
+      const response = NextResponse.next()
+      response.cookies.set('access', newAccessToken, { maxAge: 60 * 45 })
+      response.cookies.set('refresh', newRefreshToken, { maxAge: 60 * 60 * 24 * 30 })
       return response
     } else {
+      toast({ title: 'ログインしてください' })
       return NextResponse.redirect(new URL('/login', request.url))
     }
   }
@@ -26,18 +28,19 @@ async function handleToken(request: NextRequest, accessToken: string | undefined
   }
   // どちらもない場合は、ログイン画面にリダイレクトする
   if (!accessToken && !refreshToken) {
+    toast({ title: 'ログインしてください' })
     return NextResponse.redirect(new URL('/login', request.url))
   }
 }
 
 export async function middleware(request: NextRequest) {
-  const refreshToken = await request.cookies.get('refresh')?.value
-  const accessToken = await request.cookies.get('access')?.value
-  const loginUserId = await request.cookies.get('loginUserId')?.value
+  const refreshToken = request.cookies.get('refresh')?.value
+  const accessToken = request.cookies.get('access')?.value
+  const loginUserId = request.cookies.get('loginUserId')?.value
 
   if (request.nextUrl.pathname.startsWith('/login')) {
-    request.cookies.delete('access')
-    request.cookies.delete('refresh')
+    await request.cookies.delete('access')
+    await request.cookies.delete('refresh')
   }
 
   if (loginUserId && request.nextUrl.pathname.startsWith(`/profile/${loginUserId}`)) {
