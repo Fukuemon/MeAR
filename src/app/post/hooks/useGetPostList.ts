97cd7ecdb2ce7e1bbcfc6705b50d612db28f6c12
Fukuemon <Fukuemony029@gmail.com>
2024-01-23 22:52:59 +0900

    ✨ feat:  #58  ペジネーション機能実装

--- src/app/post/hooks/useGetPostList.ts
+++ src/app/post/hooks/useGetPostList.ts
@@ -1,11 +1,16 @@
 import { AxiosError } from 'axios'
 import useSWR from 'swr'
 import { fetcher } from '@/libs/fetcher/fetcher'
-import { PostListItem } from '@/types/Post/types'
+import { PostListApiResponse } from '@/types/Post/types'
 
-export const useGetPostList = () => {
-  const url = process.env.NEXT_PUBLIC_BACKEND_URL + 'post/'
-  const { data, error, isLoading, mutate } = useSWR<PostListItem[], AxiosError>(url, fetcher)
+export const useGetPostList = (pageId?: string) => {
+  if (!pageId) {
+    pageId = '1'
+  }
+  const url = process.env.NEXT_PUBLIC_BACKEND_URL + 'post/?page=' + pageId
+  console.log(url)
+  const { data, error, isLoading, mutate } = useSWR<PostListApiResponse, AxiosError>(url, fetcher)
+  console.log(data)
 
   return {
     postList: data,
